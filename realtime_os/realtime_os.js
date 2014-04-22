2bc54366f89d55a2bf30676c29b3fcd25c640ced
Priyashree S <priya_sudarshanam@harvard.edu>
2014-04-22 11:23:00 -0400

    edited js function and remove third instantiation

--- realtime_os/realtime_os.js
+++ realtime_os/realtime_os.js
@@ -1,12 +1,11 @@
 
-
  (function ($) {
   $(document).ready(function(){
     
     setInterval(function() {
 	  $( "#realtime_users" ).load( "realtime_os/active_users #realtime_users", function() {
-  $('#realtime_users').html(result);;
+ // $('#realtime_users').html(result);;
 })
-       }, 60000);
+       }, 5 * 60000);
   });
 })(jQuery);  

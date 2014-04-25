7c394f2ea6fc5cc79f61e6873d5495d8adfe3f53
Priyashree S <priya_sudarshanam@harvard.edu>
2014-04-24 22:18:17 -0400

    combining os_board and realtime in one module

--- realtime_os/realtime_os.js
+++ realtime_os/realtime_os.js
@@ -6,6 +6,19 @@
 	  $( "#realtime_users" ).load( "realtime_os/active_users #realtime_users", function() {
  // $('#realtime_users').html(result);;
 })
-       }, 5 * 60000);
+       }, 1000);
   });
+  
 })(jQuery);  
+
+
+(function ($) {
+  $(document).ready(function(){
+    
+    setInterval(function() {
+	  $( "#realtime_pages" ).load( "realtime_os/active_pages #realtime_pages", function() {
+ // $('#realtime_users').html(result);;
+})
+       }, 1000);
+  });
+})(jQuery); 

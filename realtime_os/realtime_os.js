c8e9bb2ae392a247bdc2853a4f23e2da3230c4a5
Priyashree S <priya_sudarshanam@harvard.edu>
2014-04-24 22:52:52 -0400

    combined functions

--- realtime_os/realtime_os.js
+++ realtime_os/realtime_os.js
@@ -4,21 +4,17 @@
     
     setInterval(function() {
 	  $( "#realtime_users" ).load( "realtime_os/active_users #realtime_users", function() {
- // $('#realtime_users').html(result);;
+				})
+       }, 1000);
+	   
+	 setInterval(function() {
+	  $( "#realtime_pages" ).load( "realtime_os/active_users #realtime_pages", function() {
+ 
 })
        }, 1000);
+	   
   });
   
 })(jQuery);  
 
 
-(function ($) {
-  $(document).ready(function(){
-    
-    setInterval(function() {
-	  $( "#realtime_pages" ).load( "realtime_os/active_pages #realtime_pages", function() {
- // $('#realtime_users').html(result);;
-})
-       }, 1000);
-  });
-})(jQuery); 
